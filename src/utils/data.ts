694bc71b27511438b657e7ffedde74bcd51646a9
adamn1225 <anoah1225@gmail.com>
2024-11-08 14:30:21 -0500

    first commit

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,36 +1,36 @@
 const services = [
     {
-        title: "Social media marketing",
+        title: "General Ed. and Special Ed.",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "Amazon affiliate marketing",
+        title: "Common Core Standards ",
         description: "Sunt, ipsam, necessitatibus sint fugit officia laboriosam minima ab ullam at magni et. Quaerat, sint!",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "Email marketing",
+        title: "Students with Disabilities ",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "Social media marketing",
+        title: "Spalding, Wilson & Orton-Gillingham Reading Intervention ",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "Amazon affiliate marketing",
+        title: "Dyslexia/Dysgraphia",
         description: "Sunt, ipsam, necessitatibus sint fugit officia laboriosam minima ab ullam at magni et. Quaerat, sint!",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "Email marketing",
+        title: "Five Steps of Reading Comprehension Method",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
@@ -38,3 +38,20 @@ const services = [
 ];
 
 export { services };
+
+const testimonials = [
+    {
+    title: " - Mrs. Schulman",
+    description: "“Aleph Homeschool and Tutoring has done an outstanding job Homeschooling my 2 daughters. Their Reading Fluency and Accuracy improved tremendously in a short period of time, as well as their Math Skills. I am very happy with their progress and would highly recommend this Service!!”"
+    },
+    {
+        title: " - Rebecca K.",
+        description: "“I am so thankful that I was introduced to Aleph Homeschool and Tutoring for my children. They look forward to their classes every day and are learning so much! My children learned how to Read through Aleph Homeschool and Tutoring’s Reading Program and are doing an excellent job decoding, encoding, spelling etc. I couldn’t have asked for a better teaching approach and program for my children!"
+    },
+    {
+        title: " - Mrs. R. D.",
+        description: "“My son has had an excellent learning experience with Aleph Homeschool and Tutoring. Sharon and her Team has helped him finally reach and perform on Grade Level. His Reading, Reading Comprehension, Writing and Math Skills have improved greatly. Thank You So Much!!!”"
+    }
+];
+
+export { testimonials };
